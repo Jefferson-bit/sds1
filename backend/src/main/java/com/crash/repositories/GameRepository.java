024db08c6856d284601300c163b11a4aae42659d
jefferson-bit <jhonsonbabi@hotmail.com>
2020-09-14 19:46:14 -0300

    DTO, Service

--- backend/src/main/java/com/crash/repositories/GameRepository.java
+++ backend/src/main/java/com/crash/repositories/GameRepository.java
@@ -3,7 +3,9 @@ package com.crash.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.crash.entities.Game;
+
 @Repository
-public interface GameRepository extends JpaRepository<GameRepository, Long>{
+public interface GameRepository extends JpaRepository<Game, Long>{
 
 }
